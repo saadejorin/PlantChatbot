88ac1f5978704c30951edab1214d69ede2a3934d
dev@flux <saadejorin@gmail.com>
2023-07-17 08:29:13 +0000

    Update st-superchat.py

--- st-superchat.py
+++ st-superchat.py
@@ -23,7 +23,8 @@ repo="HuggingFaceH4/starchat-beta"
 
 ### START STREAMLIT UI
 st.markdown("<h1 style='text-align: center; color: black;'>🌱PlantAI ChatBot</h1>", unsafe_allow_html=True)
-st.markdown("<h3 style='text-align: center; color: black; margin-top: -30px;'><i>using Starchat-beta</i></h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center; color: gray; margin-top: -30px;'><i>using Starchat-beta</i></h3>", unsafe_allow_html=True)
+
 
 
 
