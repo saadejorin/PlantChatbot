df9b2177f72b4d8362916aaf435a2080143e3f79
dev@flux <saadejorin@gmail.com>
2023-07-18 18:44:44 +0000

    Update st-superchat.py

--- st-superchat.py
+++ st-superchat.py
@@ -1,12 +1,13 @@
 import streamlit as st
 from huggingface_hub import InferenceClient
-from langchain import HuggingFaceHub, PromptTemplate, LLMChain
+from langchain import HuggingFaceHub
 import requests
+# Internal usage
 import os
 from time import sleep
 
 #AVATARS
-av_us = '👨‍🌾'
+av_us = '👨‍🌾'  #"🦖"  #A single emoji, e.g. "🧑‍💻", "🤖", "🦖". Shortcodes are not supported.
 av_ass = '🤖'
 
 # FUNCTION TO LOG ALL CHAT MESSAGES INTO chathistory.txt
@@ -14,22 +15,13 @@ def writehistory(text):
     with open('chathistory.txt', 'a') as f:
         f.write(text)
         f.write('\n')
+    f.close()
 
 # Set HF API token
-yourHFtoken = os.getenv('HUGGINGFACE_TOKEN') 
+yourHFtoken = os.getenv('HUGGINGFACE_TOKEN') #here your HF token
 repo="HuggingFaceH4/starchat-beta"
 
-# Initialize the model
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = yourHFtoken
-llm = HuggingFaceHub(repo_id=repo,
-                     model_kwargs={"min_length":30,
-                                   "max_new_tokens":400, "do_sample":True,
-                                   "temperature":0.2, "top_k":50,
-                                   "top_p":0.95, "eos_token_id":49155})
-prompt = PromptTemplate(template="{myprompt}\n", input_variables=["myprompt"])
-llm_chain = LLMChain(prompt=prompt, llm=llm)
-
-# START STREAMLIT UI
+### START STREAMLIT UI
 st.markdown("<h1 style='text-align: center; color: black;'>🌱PlantAI ChatBot</h1>", unsafe_allow_html=True)
 st.markdown("<h3 style='text-align: center; color: gray; margin-top: -30px;'><i>using Starchat-beta</i></h3>", unsafe_allow_html=True)
 
@@ -37,6 +29,26 @@ if st.button("What are common plant crop diseases?"): myprompt = "What are commo
 if st.button("How does a plant's immune system work?"): myprompt = "How does a plant's immune system work?"
 if st.button("What causes leaf yellowing in plants?"): myprompt = "What causes leaf yellowing in plants?"
 if st.button("How do nutrients affect plant growth?"): myprompt = "How do nutrients affect plant growth?"
+
+# Set a default model
+if "hf_model" not in st.session_state:
+    st.session_state["hf_model"] = "HuggingFaceH4/starchat-beta"
+
+### INITIALIZING STARCHAT FUNCTION MODEL
+def starchat(model,myprompt, your_template):
+    from langchain import PromptTemplate, LLMChain
+    os.environ["HUGGINGFACEHUB_API_TOKEN"] = yourHFtoken
+    llm = HuggingFaceHub(repo_id=model ,
+                         model_kwargs={"min_length":30,
+                                       "max_new_tokens":1000, "do_sample":True,
+                                       "temperature":0.2, "top_k":50,
+                                       "top_p":0.95, "eos_token_id":49155})
+    template = your_template
+    prompt = PromptTemplate(template=template, input_variables=["myprompt"])
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+    llm_reply = llm_chain.run(myprompt)
+    reply = llm_reply.partition('')[0]
+    return reply
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -64,7 +76,9 @@ if myprompt := st.chat_input("What are common plant disease?"):
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
-        res  =  llm_chain.run(myprompt) # Using the pre-loaded llm_chain
+        res  =  starchat(
+                st.session_state["hf_model"],
+                myprompt, "\n\n\n{myprompt}\n")
         response = res.split(" ")
         for r in response:
             full_response = full_response + r + " "
